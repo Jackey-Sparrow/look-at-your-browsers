57114e1120743aab09b84082eb2a2e172ca64cd3
Jackey.Sparrow <1109952026@qq.com>
2015-12-16 20:37:25 +0800

    add package

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 /**
  * Created by Jackey Li on 2015/12/15.
  */
-//
+
+var gulp = require('gulp'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    ngMin = require('gulp-ngmin');
